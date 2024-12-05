2b26aea1c3247de827f51192ebce172a705803fa
Jake Lee <leejaku@gmail.com>
2024-12-05 21:53:16 +0900

    admin 추가함.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from polls.models import Media, Board, Question, Choice
+
+
 # Register your models here.
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('type', 'url')
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_by')
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('board', 'text', 'media')
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'text', 'votes')
