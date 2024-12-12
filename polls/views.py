05b872d394528c00f7b6f78d9b5092c3b3ea0aea
Jake Lee <leejaku@gmail.com>
2024-12-13 03:22:20 +0900

    투표하기 done

--- polls/views.py
+++ polls/views.py
@@ -54,13 +54,16 @@ def board_vote(request, id):
         board = Board.objects.get(id=id)
         questions = board.question_set.all()  # board와 연결된 모든 질문 가져오기
         # 각 질문에 대한 choice를 함께 가져옴
+        ids = []
         for question in questions:
             question.choices = question.choice_set.all()  # choice들을 속성으로 추가
+            ids.append(question.id)
 
         context = {
             'sub_title': '투표하기',
             'board': board,
             'questions': questions,  # question과 그에 연결된 choice들 포함
+            'question_ids': ids
         }
         return render(request, 'polls/board_vote.html', context)
     except Board.DoesNotExist:
@@ -103,6 +106,23 @@ def api_modify_board(request):
 
         board.save()
         return JsonResponse({'id': board.id, 'name': board.name}, status=201)  # 생성된 객체 반환
+
+
+@login_required
+@csrf_exempt
+def api_vote_board(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        for item in data:
+            q = item.get('question')
+            a = item.get('answer')
+            print(f"q={q}, a={a}")
+            choice = Choice.objects.get(id=a)
+            choice.increment_votes()
+
+        return JsonResponse({'count': len(data)}, status=201)  # 생성된 객체 반환
+
 
 @login_required
 @csrf_exempt
