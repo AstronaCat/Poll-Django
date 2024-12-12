ad9b9292c595f93df0ed1926652333f07db950dd
Jake Lee <leejaku@gmail.com>
2024-12-12 19:29:25 +0900

    signup 기능 정리

--- mysite/forms.py
+++ mysite/forms.py
@@ -3,8 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='유효한 이메일 주소를 입력하세요.')
+    email = forms.EmailField(max_length=254, help_text='유효한 이메일 주소를 입력하세요.', widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # 모든 필드에 'form-control' 클래스 추가
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
